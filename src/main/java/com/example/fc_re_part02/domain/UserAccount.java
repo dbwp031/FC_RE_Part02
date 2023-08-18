f31891709894cb4ca1b9ab200b7139738846494c
Yuje Lee <dbwp031@gmail.com>
2023-08-18 16:52:16 +0900

    #3 - 도메인 UserAccount 엔티티 추가
    사용자 계정을 의미하는 UserAccount 엔티티를 추가했습니다.
    본 서비스에서 Spring Security를 사용하는데, 이때 User Class가 이미 정의되어 있기 때문에
    중복을 피하기 위하여 UserAccount로 명명하였습니다.

---
+++ src/main/java/com/example/fc_re_part02/domain/UserAccount.java
@@ -0,0 +1,68 @@
+package com.example.fc_re_part02.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class UserAccount extends AuditingFields {
+    @Id
+    @Column(length = 50)
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter
+    @Column(length = 100)
+    private String email;
+
+    @Setter
+    @Column(length = 100)
+    private String nickname;
+
+    @Setter
+    private String memo;
+
+    protected UserAccount() {
+    }
+
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
+        return UserAccount.of(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserAccount that = (UserAccount) o;
+        return Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+}
