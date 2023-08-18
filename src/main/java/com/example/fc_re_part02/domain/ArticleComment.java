3be3e2ecbb9346e3cad6fb51a5ffd0a382a6e503
Yuje Lee <dbwp031@gmail.com>
2023-08-18 16:51:04 +0900

    #3 - 도메인 ArticleComment 엔티티 추가
    댓글의 역할인 ArticleComment 엔티티를 추가하였습니다.

---
+++ src/main/java/com/example/fc_re_part02/domain/ArticleComment.java
@@ -0,0 +1,61 @@
+package com.example.fc_re_part02.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+
+/*
+* @ManyToOne(optional=false)
+* optional -> false면 절대 null일 수 없다.
+* */
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class ArticleComment extends AuditingFields{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article;
+
+    @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "userId")
+    private UserAccount userAccount;
+
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
+        this.article = article;
+        this.content = content;
+        this.userAccount = userAccount;
+    }
+
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
